d2259018463d94d3d7181aa5d747b514be3a95bd
Toungchai Sriangthong <114647008+Kingleopard168@users.noreply.github.com>
2022-10-02 13:50:48 +0700

    Palindrome number
    Palindrome number

---
+++ Palindrome_Number.py
@@ -0,0 +1,15 @@
+number=int(input("Enter number:"))
+num = number
+temp = num
+reverse = 0
+while temp > 0:
+    remainder = temp % 10
+    print(remainder, "x")
+    reverse = (reverse * 10) + remainder
+    print(reverse, "y")
+    temp = temp // 10
+    print(temp, "z")
+if num == reverse:
+  print('Palindrome')
+else:
+  print("Not Palindrome")
