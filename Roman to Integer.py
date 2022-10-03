287fb3d1266dd3df5d9a443288b1701aa4bf87a3
Toungchai Sriangthong <114647008+Kingleopard168@users.noreply.github.com>
2022-10-03 18:59:38 +0700

    นำตัวอักษรมาแทนตัวเลขแล้วนำมาบวกกันเพื่อหาผมลัพที่ต้องการหรือนำไปใช้ทำอย่างอื่น
    นำตัวอักษรมาแทนตัวเลขแล้วนำมาบวกกันเพื่อหาผมลัพที่ต้องการหรือนำไปใช้ทำอย่างอื่น

---
+++ Roman to Integer.py
@@ -0,0 +1,21 @@
+from unittest import result
+
+
+class Solution(object):
+   def romanToInt(self, s):
+      """
+      :type s: str
+      :rtype: int
+      """
+      roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+      i = 0
+      num = 0
+      while i < len(s):
+            #print(i)
+            num+=roman[s[i]]
+            i+=1
+      return num
+ob1 = Solution()
+#print("result")
+print(ob1.romanToInt("MCMXCIV"))
+#print(ob1.romanToInt("CDXLIII"))
